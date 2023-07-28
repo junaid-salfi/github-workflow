6a12a12deba1783d2cb56acc256c87a7ce21ff58
junaid-nazir <t_junaid.nazir@tatadigital.com>
2023-07-28 11:27:25 +0530

    Initial Commit

---
+++ github-workflow/github_workflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_workflowApp.swift
+//  github-workflow
+//
+//  Created by Junaid Nazir on 28/07/23.
+//
+
+import SwiftUI
+
+@main
+struct github_workflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ github-workflowUITests/github_workflowUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  github_workflowUITestsLaunchTests.swift
+//  github-workflowUITests
+//
+//  Created by Junaid Nazir on 28/07/23.
+//
+
+import XCTest
+
+final class github_workflowUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
